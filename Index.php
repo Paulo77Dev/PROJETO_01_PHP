3371828932d5228d0982f244274d8f99286c7029
Paulo77Dev <paulomadson16@gmail.com>
2022-09-17 00:55:30 -0300

    iniciando projeto em php

---
+++ Index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>PROJETO 01</title>
+</head>
+<body bgcolor="Indigo">
+<FONT COLOR="white">
+<?php  echo  '<H1><strong><p>Olá mundo, essa é a minha pagina de login</p></strong></H1> '?>
+<?=  '<p>Nos proximos dias teremos uma linda página de login,</p>para finalizar esse treinamento interno e vai ser aplicado o laravel, <strong><em>FRAMEWORK<em></strong>.' ?>
+<div class="container">
+<img src="ING1.jpg">
+</div>
+</FONT>
+</body>
+</html>
+
+<!--treino basico com php e html
